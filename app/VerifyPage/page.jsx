56419889a5f79a39c2eca2b4ef717d2f37f7ecb9
River <jahandealireza@yahoo.com>
2025-01-04 23:10:07 +0330

    Vi

---
+++ app/VerifyPage/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';  
+
+
+const MyComponent = () => {  
+  return (  
+    <div>  
+      <h1>My Next.js Component</h1>  
+      <p>This is a simple example of a Next.js component.</p>  
+    </div>  
+  );  
+};  
+
+export default MyComponent;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 
+
 import Image from "next/image";
 import logo from "./logo.png";
-
+import rules_icon from "./rules_icon.png";
+import Link from "next/link";
 
 export default function Home() {
   return <div className="flex flex-col items-center bg-green-300 px-[24px] h-[100%]">
@@ -14,8 +16,20 @@ export default function Home() {
 
            
           <p className="bg-slate-400 w-[100%] flex justify-end mt-[48px] mb-[12px]">شماره همراه خود را وارد کنید</p>
-          <input type="text" placeholder="شماره همراه" className="w-[100%] flex text-right" />           
+          <input type="text" placeholder="شماره همراه" 
+          className="w-[100%] flex text-right border rounded-[8px] px-2 h-[50px]" />           
+         
+          <div className="flex-grow bg-zinc-400"></div>
+
+          <div className="flex bg-red-50 items-center w-[100%]">
+           <p className=" w-[100%] mr-[8px] flex justify-end">استفاده از پزشکینو به معنی پذیرش تمامی قوانین و مقررات</p>
+           <Image src={rules_icon} width="28" height="28" alt="image not found"/>
+          </div>
+          <p className=" w-[100%] bg-red-50 flex justify-end mb-[16px]"> .اپلیکیش است</p>
+          <Link href="./VerifyPage" className="h-[50px] bg-[#06C1FF] w-[100%] mb-[20px] flex justify-center items-center rounded-[8px]">تایید و دریافت کد</Link>
+
+          <Link href="./VerifyPage" className="h-[50px] bg-[#06C1FF] w-[100%] mb-[20px] flex justify-center items-center rounded-[8px]">تایید و دریافت کد</Link>
          </div>
     
-  ;
+  
 }
